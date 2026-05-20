dc06425fb9f1e897d6b3a50b6784027b004d9949
Yahya2511 <y.hamza2511@gmail.com>
2026-05-20 04:27:53 +0300

    Main code with dummy data till I make database.

--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
-@app.get("/")
+# --- DUMMY DATA ---
+
+habits = [
+    {
+        "id": 1,
+        "name": "Study",
+        "description": "You need to become better",
+        "frequency_type": "daily",
+        "goal_value": 4,
+        "created_at": "2026-05-18T08:00:00"
+    },
+    {
+        "id": 2,
+        "name": "Read",
+        "description": "Read any book",
+        "frequency_type": "daily",
+        "goal_value": 30,
+        "created_at": "2026-05-19T09:30:00"
+    }
+]
+
+habit_logs = [
+    {
+        "id": 1,
+        "habit_id": 1,
+        "logged_date": "2026-05-20",
+        "is_completed": True
+    },
+    {
+        "id": 2,
+        "habit_id": 2,
+        "logged_date": "2026-05-20",
+        "is_completed": False
+    }
+]
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/habits", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "hello world"}
+    return f"<h1>{habits[0]['name']}</h1>"
+
+@app.get("/api/habits")
+def get_habits():
+    return habits
